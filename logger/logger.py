3c996562d699e5f15699dc31b0877211cbd3242e
Dominic Leung <leungcheuk209@hotmail.com>
2023-12-17 11:44:19 +0800

    clean codes

--- logger/logger.py
+++ logger/logger.py
@@ -9,11 +9,11 @@ logger.setLevel(logging.DEBUG)
 script_dir = os.path.dirname(os.path.realpath(__file__))
 log_file = os.path.join(script_dir, 'file.log')
 file_handler = logging.FileHandler(log_file, mode = "w")
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
 
+# Create StreamHandler
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
-
 
 # Create formatters and add it to handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
